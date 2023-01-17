bb3e6c2f4d58c361317ad59944fa962198c3342b
harry-lkl <harryk742121@gmail.com>
2023-01-17 19:26:53 +0000

    complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,15 @@
 const repeatString = function(str, num) {
-    let outputString = '';
-    for (let i = 0; i <= num - 1; i++) {
-        outputString += str;
-    }
-    return outputString;
-};
+    if (num < 0){
+        return 'ERROR';
+        }
+    else {
+        let outputString = '';
+        for (let i = 0; i <= num - 1; i++) {
+            outputString += str;
+        }
+        return outputString;
+        }
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
