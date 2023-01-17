18bf54db9f2abf03b7a32020621425d13dcb5d60
harry-lkl <harryk742121@gmail.com>
2023-01-17 19:19:22 +0000

    complete exercise 01 and start 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let outputString = '';
+    for (let i = 0; i <= num - 1; i++) {
+        outputString += str;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
