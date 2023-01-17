26ae39f5335e84d7840e1bbc28da6638e639063b
harry-lkl <harryk742121@gmail.com>
2023-01-17 19:34:47 +0000

    complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // split string into array and save into a constant
+    // reverse array
+    // join and return the array
+    const wordArray = str.split("");
+    wordArray.reverse();
+    return wordArray.join("");
 };
 
 // Do not edit below this line
