c11215591530bba6b968b2cc9d8ee15f65a31791
harry-lkl <harryk742121@gmail.com>
2023-01-18 15:31:56 +0000

    complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+// (32°F − 32) × 5/9 = 0°C
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5 / 9)
+  return (Math.round(celsius * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+// (0°C × 9/5) + 32 = 32°F
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = celsius * (9 / 5) + 32;
+  return (Math.round(fahrenheit * 10) / 10);
 };
 
 // Do not edit below this line
