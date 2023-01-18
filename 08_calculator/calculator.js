cb80c1bfad856aaae2ae160e40302a05eba86d04
harry-lkl <harryk742121@gmail.com>
2023-01-18 16:21:23 +0000

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0;
+  } else {
+	  return array.reduce((total,num) => total + num);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length == 0) {
+    return 0;
+  } else {
+	  return array.reduce((total,num) => total * num);
+  }
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let number = 1;
+	for (let i = 1; i <= a; i++) {
+    number *= i;
+  }
+  return number;
+
+  // can also use recursion:
+  // if a === 0, return 1;
+  // return a * factorial(a - 1);  
 };
 
 // Do not edit below this line
