fe196a5335e11436fec440de1a65590c35ebe92b
harry-lkl <harryk742121@gmail.com>
2023-01-18 13:50:40 +0000

    complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
 
-};
+    return array.filter(element => !arg.includes(element));
+
+}
+// ...arg passes all the arguments into an array
+// includes checks if this array contains the element, and returns true if the element is a match
+//      return 'not true' with '!' 
+// filter() returns a new array with elements that are 'true',
+//      which is 'not a match in ...arg' in this case
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
