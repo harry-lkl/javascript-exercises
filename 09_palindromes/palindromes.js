d699493f58a314d57cf3f9c68fff6cbdb8b18739
harry-lkl <harryk742121@gmail.com>
2023-01-19 15:42:58 +0000

    complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+// a function that determines whether or not a given string is a palindrome
+    // take in a string
+    // split string into an array
+    // reverse the array
+    // join the array and store in a const
+    // compare the to strings
 
+const palindromes = function (str) {
+    const cleanString = str.match(/[a-z]/gi).join('').toLowerCase();
+    const reversedString = str.match(/[a-z]/gi).reverse().join('').toLowerCase();
+    return reversedString === cleanString;
 };
 
 // Do not edit below this line
